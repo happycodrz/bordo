8c906a2751036e9c040c234f0884300f626469e8
Kevin Brown <chevinbrown@gmail.com>
2020-02-21 15:24:16 -0600

    Add httpoison

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Bordo.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:httpoison, "~> 1.6.2"},
       {:phoenix, "~> 1.4.12"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
