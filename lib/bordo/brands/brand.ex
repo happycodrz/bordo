b18c687c841fe3775103f8e5d00cf078647680f6
Kevin Brown <chevinbrown@gmail.com>
2020-02-27 08:56:51 -0600

    Remove icon_url

--- lib/bordo/brands/brand.ex
+++ lib/bordo/brands/brand.ex
@@ -4,7 +4,6 @@ defmodule Bordo.Brands.Brand do
   import Bordo.Schema, only: [generate_short_uuid: 0]
 
   schema "brands" do
-    field :icon_url, :string
     field :name, :string
     field :uuid, :string
 
@@ -14,8 +13,8 @@ defmodule Bordo.Brands.Brand do
   @doc false
   def changeset(brand, attrs) do
     brand
-    |> cast(attrs, [:name, :icon_url])
+    |> cast(attrs, [:name])
     |> put_change(:uuid, generate_short_uuid())
-    |> validate_required([:name, :icon_url])
+    |> validate_required([:name])
   end
 end
