c71747b8667447af3a0b1e0f97a91e5af771b442
Kevin Brown <chevinbrown@gmail.com>
2020-03-03 20:26:38 -0600

    Add user_id

--- lib/bordo/auth/identity.ex
+++ lib/bordo/auth/identity.ex
@@ -1,11 +1,13 @@
 defmodule Auth.Identity do
   @moduledoc """
   This struct represents the Identitiy accessible on each connection
+  It contains an id(auth0 id) and a user_id.
   """
-  @enforce_keys [:id]
-  defstruct id: nil
+  @enforce_keys [:id, :user_id]
+  defstruct id: nil, user_id: nil
 
   @type t() :: [
-          id: String.t()
+          id: String.t(),
+          user_id: String.t()
         ]
 end
