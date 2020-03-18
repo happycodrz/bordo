3af429a8117a7f2937dad9e8153af8d6129ba72e
Kevin Brown <chevinbrown@gmail.com>
2020-03-18 17:47:16 -0500

    Cleanup functions becuase they're structured data

--- lib/bordo_web/current_identity.ex
+++ lib/bordo_web/current_identity.ex
@@ -1,19 +1,5 @@
 defmodule BordoWeb.CurrentIdentity do
-  def current_identity(conn) do
-    %Plug.Conn{
-      assigns: %{current_identity: current_identity}
-    } = conn
-
-    current_identity
-  end
-
-  def user_id(conn) do
-    %Auth.Identity{user_id: user_id} = conn |> current_identity()
-    user_id
-  end
-
-  def team_id(conn) do
-    %Auth.Identity{team_id: team_id} = conn |> current_identity()
-    team_id
-  end
+  def current_identity(conn), do: conn.assigns.current_identity
+  def user_id(conn), do: current_identity(conn).user_id
+  def team_id(conn), do: current_identity(conn).team_id
 end
