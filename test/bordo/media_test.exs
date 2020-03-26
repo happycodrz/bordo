834bb417406663ccfe29a6169b63b9e0a01a041b
Kevin Brown <chevinbrown@gmail.com>
2020-03-26 15:26:20 -0500

    Remove dup-generated tests

--- test/bordo/media_test.exs
+++ test/bordo/media_test.exs
@@ -4,73 +4,18 @@ defmodule Bordo.MediaTest do
   alias Bordo.Media
 
   describe "images" do
-    alias Bordo.Media.Images
-
-    @valid_attrs %{name: "some name", s3_object_name: "some s3_object_name", s3_path: "some s3_path"}
-    @update_attrs %{name: "some updated name", s3_object_name: "some updated s3_object_name", s3_path: "some updated s3_path"}
-    @invalid_attrs %{name: nil, s3_object_name: nil, s3_path: nil}
-
-    def images_fixture(attrs \\ %{}) do
-      {:ok, images} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Media.create_images()
-
-      images
-    end
-
-    test "list_images/0 returns all images" do
-      images = images_fixture()
-      assert Media.list_images() == [images]
-    end
-
-    test "get_images!/1 returns the images with given id" do
-      images = images_fixture()
-      assert Media.get_images!(images.id) == images
-    end
-
-    test "create_images/1 with valid data creates a images" do
-      assert {:ok, %Images{} = images} = Media.create_images(@valid_attrs)
-      assert images.name == "some name"
-      assert images.s3_object_name == "some s3_object_name"
-      assert images.s3_path == "some s3_path"
-    end
-
-    test "create_images/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Media.create_images(@invalid_attrs)
-    end
-
-    test "update_images/2 with valid data updates the images" do
-      images = images_fixture()
-      assert {:ok, %Images{} = images} = Media.update_images(images, @update_attrs)
-      assert images.name == "some updated name"
-      assert images.s3_object_name == "some updated s3_object_name"
-      assert images.s3_path == "some updated s3_path"
-    end
-
-    test "update_images/2 with invalid data returns error changeset" do
-      images = images_fixture()
-      assert {:error, %Ecto.Changeset{}} = Media.update_images(images, @invalid_attrs)
-      assert images == Media.get_images!(images.id)
-    end
-
-    test "delete_images/1 deletes the images" do
-      images = images_fixture()
-      assert {:ok, %Images{}} = Media.delete_images(images)
-      assert_raise Ecto.NoResultsError, fn -> Media.get_images!(images.id) end
-    end
-
-    test "change_images/1 returns a images changeset" do
-      images = images_fixture()
-      assert %Ecto.Changeset{} = Media.change_images(images)
-    end
-  end
-
-  describe "images" do
     alias Bordo.Media.Image
 
-    @valid_attrs %{name: "some name", s3_object_name: "some s3_object_name", s3_path: "some s3_path"}
-    @update_attrs %{name: "some updated name", s3_object_name: "some updated s3_object_name", s3_path: "some updated s3_path"}
+    @valid_attrs %{
+      name: "some name",
+      s3_object_name: "some s3_object_name",
+      s3_path: "some s3_path"
+    }
+    @update_attrs %{
+      name: "some updated name",
+      s3_object_name: "some updated s3_object_name",
+      s3_path: "some updated s3_path"
+    }
     @invalid_attrs %{name: nil, s3_object_name: nil, s3_path: nil}
 
     def image_fixture(attrs \\ %{}) do
