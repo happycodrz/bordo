85fd40cf4100eaebe99afa8ed133d4e5cbc9fd21
Kevin Brown <chevinbrown@gmail.com>
2020-04-08 18:02:50 -0500

    Create channel from callback & cleanup

--- lib/bordo_web/controllers/providers/twitter_controller.ex
+++ lib/bordo_web/controllers/providers/twitter_controller.ex
@@ -1,13 +1,23 @@
 defmodule BordoWeb.Providers.TwitterController do
   use BordoWeb, :controller
+  alias Bordo.Brands.Brand
+  alias Bordo.Channels
+  alias Bordo.Channels.Channel
 
-  def auth(conn, _params) do
+  def auth(conn, %{"brand_id" => brand_id}) do
     ExTwitter.configure(:process,
       consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
       consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET")
     )
 
-    tokens = ExTwitter.request_token()
+    query = URI.encode_query(%{brand_id: brand_id})
+
+    tokens =
+      System.get_env("TWITTER_CALLBACK_URI")
+      |> URI.parse()
+      |> Map.merge(%{query: query})
+      |> URI.to_string()
+      |> ExTwitter.request_token()
 
     # Generate the url for "Sign-in with twitter".
     # For "3-legged authorization" use ExTwitter.authorize_url instead
@@ -17,7 +27,8 @@ defmodule BordoWeb.Providers.TwitterController do
 
   def callback(conn, %{
         "oauth_token" => oauth_token,
-        "oauth_verifier" => oauth_verifier
+        "oauth_verifier" => oauth_verifier,
+        "brand_id" => brand_id
       }) do
     ExTwitter.configure(:process,
       consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
@@ -26,10 +37,29 @@ defmodule BordoWeb.Providers.TwitterController do
 
     # Exchange for an access token
     with {:ok, access_token} <- ExTwitter.access_token(oauth_verifier, oauth_token) do
-      json(conn, %{
-        "token" => access_token.oauth_token,
-        "secret" => access_token.oauth_token_secret
-      })
+      brand = Bordo.Repo.get_by!(Brand, uuid: brand_id)
+      IO.inspect(access_token, label: "ACCESS TOKEN")
+
+      channel_params =
+        Map.merge(
+          %{
+            "token" => access_token.oauth_token,
+            "token_secret" => access_token.oauth_token_secret,
+            "network" => "twitter"
+          },
+          %{"brand_id" => brand.id}
+        )
+
+      with {:ok, %Channel{} = channel} <- Channels.create_channel(channel_params) do
+        conn
+        |> put_status(:created)
+        |> put_resp_header(
+          "location",
+          Routes.brand_channel_path(conn, :show, brand_id, channel)
+        )
+        |> put_view(BordoWeb.Brands.ChannelView)
+        |> render("show.json", channel: channel)
+      end
     else
       {:error, 401} ->
         json(conn, %{errors: %{detail: "auth failed"}})
