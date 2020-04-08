4eebd76f2e65945469d3d873947874f0ead1bd36
Kevin Brown <chevinbrown@gmail.com>
2020-04-08 18:03:16 -0500

    Add module to start provider interation

---
+++ lib/bordo/providers/twitter.ex
@@ -0,0 +1,16 @@
+defmodule Bordo.Providers.Twitter do
+  def create_tweet(channel, status) do
+    ExTwitter.configure(:process,
+      consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
+      consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
+      access_token: channel.token,
+      access_token_secret: channel.token_secret
+    )
+
+    ExTwitter.update(status)
+  end
+
+  def destroy_tweet(tweet_id) do
+    ExTwitter.destroy_status(tweet_id)
+  end
+end
