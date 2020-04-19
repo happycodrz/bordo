496f033f903c0f5c8438293dbe5ae3e25a7a3d11
Kevin Brown <chevinbrown@gmail.com>
2020-04-19 13:12:32 -0500

    Convert token fields to text
    Some tokens are longer than 255 characters, so these should be text
    fields. Luckily, in postgres, text and varchar(255) have the same
    performance characteristics.

--- priv/repo/migrations/20200324220955_create_channels.exs
+++ priv/repo/migrations/20200324220955_create_channels.exs
@@ -4,8 +4,8 @@ defmodule Bordo.Repo.Migrations.CreateChannels do
   def change do
     create table(:channels, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :token, :string
-      add :token_secret, :string
+      add :token, :text
+      add :token_secret, :text
       add :user_id, :string
       add :network, :string
       add :brand_id, references(:brands, type: :uuid, on_delete: :delete_all)
