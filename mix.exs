bf12e49865d4eeeec30a99bad083c44ad01dc1b7
Kevin Brown <chevinbrown@gmail.com>
2020-04-18 19:03:23 -0500

    Add ecto_autoslug_field

--- mix.exs
+++ mix.exs
@@ -37,6 +37,7 @@ defmodule Bordo.MixProject do
       {:cloudex, "~> 1.4"},
       {:cors_plug, "~> 2.0"},
       {:credo, "~> 1.3.2", only: [:dev, :test], runtime: false},
+      {:ecto_autoslug_field, "~> 2.0"},
       {:ecto_sql, "~> 3.4"},
       {:ex_aws, "~> 2.1"},
       {:ex_aws_s3, "~> 2.0"},
