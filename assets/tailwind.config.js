13aec2d42de663c476bc4a6c36e6983e311f3ba2
Kevin Brown <chevinbrown@gmail.com>
2020-05-04 17:58:38 -0500

    Add assets
    - tailwind
    - tailwind ui
    - alpinejs
    - phoenix_live_view

---
+++ assets/tailwind.config.js
@@ -0,0 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: [
+    '../**/*.html.eex',
+    '../**/*.html.leex',
+    '../**/views/**/*.ex',
+    '../**/live/**/*.ex',
+    './js/**/*.js',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [require('@tailwindcss/ui')],
+}
