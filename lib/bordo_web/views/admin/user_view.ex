5f3c10c5fd26255cb6f9a4bccd6f07203ef7e7d3
Kevin Brown <chevinbrown@gmail.com>
2020-05-08 21:25:15 -0500

    Add admin user-view for avatar helpers

---
+++ lib/bordo_web/views/admin/user_view.ex
@@ -0,0 +1,13 @@
+defmodule BordoWeb.Admin.UserView do
+  use BordoWeb, :view
+
+  def avatar(%{image_url: image_url}) when is_binary(image_url) do
+    image_url
+  end
+
+  def avatar(%{email: email, image_url: image_url}) when is_nil(image_url) do
+    hash = :crypto.hash(:md5, email) |> Base.encode16(case: :lower)
+    default = "mm"
+    "https://www.gravatar.com/avatar/" <> hash <> "?d=" <> default
+  end
+end
