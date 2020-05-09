94198fe7e81f09ff899d9e445577a6a8b79f367d
Kevin Brown <chevinbrown@gmail.com>
2020-05-09 07:41:16 -0500

    Make select more robust
    Handle multiple types of selections so we can expand.

--- lib/bordo_web/live/admin/select.ex
+++ lib/bordo_web/live/admin/select.ex
@@ -1,26 +1,30 @@
 defmodule BordoWeb.Admin.Select do
   use BordoWeb, :live_component
-  alias Bordo.Teams
+  alias Bordo.{Teams, Users}
   import Phoenix.HTML.Form
+
+  def render(%{selection: "admin_list_users"} = assigns) do
+    selection = admin_list_users()
+    render_select(assigns, :owner_id, selection)
+  end
+
+  def render(%{selection: "admin_list_teams"} = assigns) do
+    selection = teams()
+    render_select(assigns, :team_id, selection)
+  end
+
+  defp render_select(assigns, form_for, selection) do
+    ~L"""
+      <%= select(assigns.form, form_for, selection, class: "js-choice") %>
+    """
+  end
 
   defp teams do
     Teams.list_teams() |> Enum.sort_by(& &1.name) |> Enum.map(fn team -> {team.name, team.id} end)
   end
 
-  def mount(_params, _session, socket) do
-    socket =
-      socket
-      |> assign(:teams, teams())
-      |> assign(:team, nil)
-
-    {:ok, socket}
-  end
-
-  def render(assigns) do
-    ~L"""
-      <div phx-update="ignore">
-        <%= select(assigns.form, :team_id, teams(), class: "js-choice") %>
-      </div>
-    """
+  defp admin_list_users() do
+    Users.list_users()
+    |> Enum.map(fn user -> {BordoWeb.Admin.UserView.full_name(user), user.id} end)
   end
 end
