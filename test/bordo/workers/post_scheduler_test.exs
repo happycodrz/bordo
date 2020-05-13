1056fc89ce1c92ac46877f22aac8d504e1ee1d18
Kevin Brown <chevinbrown@gmail.com>
2020-05-13 16:29:20 -0500

    Add stupid smoke test

---
+++ test/bordo/workers/post_scheduler_test.exs
@@ -0,0 +1,33 @@
+defmodule Bordo.Workers.PostSchedulerTest do
+  use Bordo.DataCase
+  use Oban.Testing, repo: Bordo.Repo
+
+  # Setup a complete post
+  setup do
+    # default channel is twitter
+    channel = fixture(:channel)
+    post_variant = %{content: "content", channel_id: channel.id}
+    post = fixture(:post, post_variants: [post_variant])
+    {:ok, post: post}
+  end
+
+  # super happy-path just for a smoke test
+  # still a useless test
+  describe "#perform" do
+    test "should create a tweet", %{post: post} do
+      post = Bordo.Posts.get_post!(post.id)
+      post_variant = Enum.at(post.post_variants, 0)
+      assert post_variant.status == "scheduled"
+
+      job =
+        Bordo.Workers.PostScheduler.new(%{"post_id" => post.id})
+        |> Oban.insert!()
+
+      Bordo.Workers.PostScheduler.perform(%{"post_id" => post.id}, job)
+
+      post = Bordo.Posts.get_post!(post.id)
+      post_variant = Enum.at(post.post_variants, 0)
+      assert post_variant.status == "published"
+    end
+  end
+end
