7a488f26a2023e8f57926373a77a2dad6a6b2d7d
Kevin Brown <chevinbrown@gmail.com>
2020-05-21 22:38:25 -0500

    Clean up login

--- lib/bordo_web/controllers/admin/login_controller.ex
+++ lib/bordo_web/controllers/admin/login_controller.ex
@@ -5,23 +5,10 @@ defmodule BordoWeb.LoginController do
   alias Auth.Credentials
 
   def index(conn, _params) do
-    changeset = Bordo.Users.change_user(%User{})
-    maybe_user = Auth.Guardian.Plug.current_resource(conn)
-
-    message =
-      if maybe_user != nil do
-        "Someone is logged in"
-      else
-        "No one is logged in"
-      end
-
     conn
-    |> put_flash(:info, message)
     |> render("index.html",
-      changeset: changeset,
-      action: Routes.login_path(conn, :login),
-      maybe_user: maybe_user,
-      flash: nil
+      errors: nil,
+      action: Routes.login_path(conn, :login)
     )
   end
 
@@ -32,8 +19,10 @@ defmodule BordoWeb.LoginController do
 
   defp login_reply({:error, error}, conn) do
     conn
-    |> put_flash(:error, error)
-    |> redirect(to: "/login")
+    |> render("index.html",
+      errors: error,
+      action: Routes.login_path(conn, :login)
+    )
   end
 
   defp login_reply({:ok, _token_resp, user}, conn) do
