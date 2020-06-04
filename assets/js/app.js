dcf9d178ddef3ce965ff6c321b9f97ef348a63a1
Kevin Brown <chevinbrown@gmail.com>
2020-06-04 17:18:45 -0500

    Prototype brand-modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,42 +25,47 @@ Hooks.FeatherIcon = {
     feather.replace()
   },
 }
-// Hooks.ScrollLock = {
-//   mounted() {
-//     this.lockScroll()
-//   },
-//   destroyed() {
-//     this.unlockScroll()
-//   },
-//   lockScroll() {
-//     // From https://github.com/excid3/tailwindcss-stimulus-components/blob/master/src/modal.js
-//     // Add right padding to the body so the page doesn't shift when we disable scrolling
-//     const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth
-//     document.body.style.paddingRight = `${scrollbarWidth}px`
-//     // Save the scroll position
-//     this.scrollPosition = window.pageYOffset || document.body.scrollTop
-//     // Add classes to body to fix its position
-//     document.body.classList.add('fix-position')
-//     // Add negative top position in order for body to stay in place
-//     document.body.style.top = `-${this.scrollPosition}px`
-//   },
-//   unlockScroll() {
-//     // From https://github.com/excid3/tailwindcss-stimulus-components/blob/master/src/modal.js
-//     // Remove tweaks for scrollbar
-//     document.body.style.paddingRight = null
-//     // Remove classes from body to unfix position
-//     document.body.classList.remove('fix-position')
-//     // Restore the scroll position of the body before it got locked
-//     document.documentElement.scrollTop = this.scrollPosition
-//     // Remove the negative top inline style from body
-//     document.body.style.top = null
-//   },
-// }
+Hooks.ScrollLock = {
+  mounted() {
+    this.lockScroll()
+  },
+  destroyed() {
+    this.unlockScroll()
+  },
+  lockScroll() {
+    // From https://github.com/excid3/tailwindcss-stimulus-components/blob/master/src/modal.js
+    // Add right padding to the body so the page doesn't shift when we disable scrolling
+    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth
+    document.body.style.paddingRight = `${scrollbarWidth}px`
+    // Save the scroll position
+    this.scrollPosition = window.pageYOffset || document.body.scrollTop
+    // Add classes to body to fix its position
+    document.body.classList.add('fix-position')
+    // Add negative top position in order for body to stay in place
+    document.body.style.top = `-${this.scrollPosition}px`
+  },
+  unlockScroll() {
+    // From https://github.com/excid3/tailwindcss-stimulus-components/blob/master/src/modal.js
+    // Remove tweaks for scrollbar
+    document.body.style.paddingRight = null
+    // Remove classes from body to unfix position
+    document.body.classList.remove('fix-position')
+    // Restore the scroll position of the body before it got locked
+    document.documentElement.scrollTop = this.scrollPosition
+    // Remove the negative top inline style from body
+    document.body.style.top = null
+  },
+}
 
 import Choices from 'choices.js'
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute('content')
 let liveSocket = new LiveSocket('/live', Socket, {
   hooks: Hooks,
+  dom: {
+    onBeforeElUpdated(from, to){
+      if(from.__x){ window.Alpine.clone(from.__x, to) }
+    }
+  },
   params: { _csrf_token: csrfToken },
 })
 
--- lib/bordo_web/live/admin/components/modal.ex
+++ lib/bordo_web/live/admin/components/modal.ex
@@ -5,44 +5,63 @@ defmodule BordoWeb.Components.ModalLive do
     {:ok, socket}
   end
 
-  def update(%{id: _id} = assigns, socket) do
-    {:ok, assign(socket, assigns)}
-  end
+  # def update(%{id: _id} = assigns, socket) do
+  #   {:ok, assign(socket, assigns)}
+  # end
 
   def render(assigns) do
+    modal_id = assigns.id
+
     ~L"""
-    <div class="fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center" phx-click="modal-close">
-    <!--
-    Background overlay, show/hide based on modal state.
+    <div>
+      <%= if @show_modal do %>
+        <div class="fixed bottom-0 inset-x-0 z-10 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center" >
+        <!--
+        Background overlay, show/hide based on modal state.
 
-    Entering: "ease-out duration-300"
-      From: "opacity-0"
-      To: "opacity-100"
-    Leaving: "ease-in duration-200"
-      From: "opacity-100"
-      To: "opacity-0"
-    -->
-    <div class="fixed inset-0 transition-opacity">
-    <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-    </div>
-
-    <!--
-    Modal panel, show/hide based on modal state.
-
-    Entering: "ease-out duration-300"
-      From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-      To: "opacity-100 translate-y-0 sm:scale-100"
-    Leaving: "ease-in duration-200"
-      From: "opacity-100 translate-y-0 sm:scale-100"
-      To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-    -->
-    <div class="bg-white rounded-lg px-4 pt-5 pb-4 overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full sm:p-6" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-      <div class="sm:flex sm:items-start">
-        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-          <%= @inner_content.([]) %>
+        Entering: "ease-out duration-300"
+          From: "opacity-0"
+          To: "opacity-100"
+        Leaving: "ease-in duration-200"
+          From: "opacity-100"
+          To: "opacity-0"
+        -->
+        <div class="fixed inset-0 transition-opacity" phx-hook="ScrollLock" phx-click="modal-close" id="modal-<%= modal_id %>" phx-target="#modal-<%= modal_id %>">
+        <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
-      </div>
+
+        <!--
+        Modal panel, show/hide based on modal state.
+
+        Entering: "ease-out duration-300"
+          From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          To: "opacity-100 translate-y-0 sm:scale-100"
+        Leaving: "ease-in duration-200"
+          From: "opacity-100 translate-y-0 sm:scale-100"
+          To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        -->
+        <div class="bg-white rounded-lg px-4 pt-5 pb-4 overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full sm:p-6" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+          <div class="sm:flex sm:items-start">
+            <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+              <%= @inner_content.([]) %>
+            </div>
+          </div>
+        </div>
+      <% end %>
     </div>
     """
+  end
+
+  def handle_event("modal-close", %{}, socket) do
+    # send(
+    #   self(),
+    #   {:button_clicked}
+    # )
+
+    {:noreply, assign(socket, show_modal: false)}
+  end
+
+  def handle_info({:button_clicked}, socket) do
+    {:noreply, assign(socket, show_modal: false)}
   end
 end
