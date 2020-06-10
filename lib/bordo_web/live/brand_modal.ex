8d12687d1e0c9e5a1941e75ea2b8b2c645c17fa4
Michael Panik <michael@hellobordo.com>
2020-06-09 20:07:49 -0500

    Update brand buttons with interactivity

--- lib/bordo_web/live/brand_modal.ex
+++ lib/bordo_web/live/brand_modal.ex
@@ -13,7 +13,7 @@ defmodule BordoWeb.BrandModal do
     <div>
       <div class="cursor-pointer">
         <div
-          class="bg-white opacity-25 h-12 w-12 flex items-center justify-center text-black text-2xl font-semibold rounded-lg mb-1 overflow-hidden" phx-click="modal-open" phx-target="<%= @myself %>">
+          class="bg-blue-700 hover:bg-blue-600 transition duration-150 h-12 w-12 flex items-center justify-center text-blue-800 text-2xl font-semibold rounded-lg mb-1 overflow-hidden" phx-click="modal-open" phx-target="<%= @myself %>">
           <svg class="fill-current h-10 w-10 block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
             <path
               d="M16 10c0 .553-.048 1-.601 1H11v4.399c0 .552-.447.601-1 .601-.553 0-1-.049-1-.601V11H4.601C4.049 11 4 10.553 4 10c0-.553.049-1 .601-1H9V4.601C9 4.048 9.447 4 10 4c.553 0 1 .048 1 .601V9h4.399c.553 0 .601.447.601 1z" />
