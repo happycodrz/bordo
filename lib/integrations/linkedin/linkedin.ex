d3f6aff4cd2521b77619be211716e5df72091c3c
Kevin Brown <chevinbrown@gmail.com>
2020-06-16 07:46:33 -0500

    Extract Linkedin methods

--- lib/integrations/linkedin/linkedin.ex
+++ lib/integrations/linkedin/linkedin.ex
@@ -12,6 +12,74 @@ defmodule Linkedin do
     end
   end
 
+  def auth_url(opts \\ []) do
+    state = Access.get(opts, :state) || %{}
+
+    query =
+      URI.encode_query(%{
+        state: URI.encode_query(state),
+        client_id: System.get_env("LINKEDIN_CLIENT_ID"),
+        redirect_uri: System.get_env("LINKEDIN_REDIRECT_URI"),
+        response_type: "code",
+        scope: "w_member_social,r_liteprofile,rw_organization_admin"
+      })
+
+    auth_url =
+      %URI{
+        host: "www.linkedin.com",
+        path: "/oauth/v2/authorization",
+        port: 443,
+        query: query,
+        scheme: "https"
+      }
+      |> URI.to_string()
+  end
+
+  def access_token(code) do
+    query =
+      URI.encode_query(%{
+        code: code,
+        client_id: System.get_env("LINKEDIN_CLIENT_ID"),
+        redirect_uri: System.get_env("LINKEDIN_REDIRECT_URI"),
+        client_secret: System.get_env("LINKEDIN_CLIENT_SECRET"),
+        grant_type: "authorization_code"
+      })
+
+    uri = %URI{
+      host: "www.linkedin.com",
+      path: "/oauth/v2/accessToken",
+      port: 443,
+      query: query,
+      scheme: "https"
+    }
+
+    with {:ok, response} <- get_token(uri) do
+      response
+      |> Map.get(:body)
+      |> Jason.decode()
+    end
+  end
+
+  def list_companies(token) do
+    HTTPoison.get!("https://api.linkedin.com/v2/organizationalEntityAcls?q=roleAssignee",
+      Authorization: "Bearer #{token}"
+    )
+    |> Map.get(:body)
+    |> Jason.decode()
+  end
+
+  def get_organization(token, org_id) do
+    HTTPoison.get!("https://api.linkedin.com/v2/organizations/#{org_id}",
+      Authorization: "Bearer #{token}"
+    )
+    |> Map.get(:body)
+    |> Jason.decode!()
+  end
+
+  defp get_token(uri) do
+    uri |> URI.to_string() |> HTTPoison.post("", [{"Content-Type", "x-www-form-urlencoded"}])
+  end
+
   def process_response_body(body), do: Jason.decode(body)
 
   def process_url(url) do
