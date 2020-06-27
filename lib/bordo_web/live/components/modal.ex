d4fe176b650b63ab357d74b6ebb0965c937b3607
Kevin Brown <chevinbrown@gmail.com>
2020-06-27 18:13:44 -0500

    Add spacing

--- lib/bordo_web/live/components/modal.ex
+++ lib/bordo_web/live/components/modal.ex
@@ -37,14 +37,16 @@ defmodule BordoWeb.Components.Modal do
           <div x-show="open" x-transition:enter="ease-out duration-100" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-100" x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
             <div @click.away="open = false" class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
-                <button type="button" @click="open = false" class="text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 transition ease-in-out duration-100">
+                <button type="button" @click="open = false" class="text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 transition ease-in-out duration-100 mb-8">
                   <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
                   </svg>
                 </button>
               </div>
               <!-- CONTENT -->
-              <%= @inner_content.([]) %>
+              <div class="mt-4">
+                <%= @inner_content.([]) %>
+              </div>
             </div>
           </div>
         </div>
