3757e8a72b39073ef62774fc4f69e3195d8aa70c
Kevin Brown <chevinbrown@gmail.com>
2020-06-27 17:59:49 -0500

    Refactor to use modal component

--- lib/bordo_web/live/brand_modal.ex
+++ lib/bordo_web/live/brand_modal.ex
@@ -4,39 +4,75 @@ defmodule BordoWeb.BrandModal do
   alias Bordo.Brands
   alias Bordo.Brands.Brand
 
-  def render(assigns) do
+  def mount(socket) do
     changeset = Brand.changeset(%Brand{}, %{})
 
+    {:ok,
+     assign(socket,
+       state: "CLOSED",
+       action: nil,
+       changeset: changeset
+     )}
+  end
+
+  def render(assigns) do
     ~L"""
-    <div>
+    <div id="modal-parent">
       <div class="cursor-pointer">
         <div
-          class="bg-blue-700 hover:bg-blue-600 transition duration-150 h-12 w-12 flex items-center justify-center text-blue-800 text-2xl font-semibold rounded-lg mb-1 overflow-hidden" phx-click="modal-open" phx-target="<%= @myself %>">
+          class="bg-blue-700 hover:bg-blue-600 transition duration-150 h-12 w-12 flex items-center justify-center text-blue-800 text-2xl font-semibold rounded-lg mb-1 overflow-hidden" phx-click="open-modal" phx-value-id="brand-modal">
           <%= feather_icon("plus", "w-56") %>
         </div>
       </div>
-      <%= if @show_modal do %>
-        <%= BordoWeb.SharedView.render("new_brand_modal.html", changeset: changeset, myself: @myself) %>
+      <%= live_component @socket, BordoWeb.Components.Modal, id: @id do %>
+        <%= f = form_for @changeset, "#", [phx_submit: :save, phx_target: "#modal-parent"] %>
+          <div class="grid grid-cols-6 gap-6 mb-4">
+            <div class="col-span-12">
+              <%= text_input f, :name, class: "mt-1 form-input text-black block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5", autofocus: true, placeholder: "New Brand Name" %>
+              <%= hidden_input f, :owner_id, value: @owner_id %>
+              <%= hidden_input f, :team_id, value: @team_id %>
+              <%= error_tag f, :name %>
+            </div>
+          </div>
+          <div class="border-t border-gray-200 pt-2">
+            <div class="flex justify-end">
+              <span class="inline-flex rounded-md shadow-sm">
+                <button type="button"
+                  class="py-2 px-4 border border-gray-300 rounded-md text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out"
+                  phx-click="close-modal" phx-target="#modal-parent" phx-value-id="<%= @id %>" @click="open = false">
+                  Cancel
+                </button>
+              </span>
+              <span class="ml-3 inline-flex rounded-md shadow-sm">
+                <button type="submit"
+                  class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-red-500 hover:bg-red-400 focus:outline-none focus:border-red-600 focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">
+                  Add New Brand
+                </button>
+              </span>
+            </div>
+          </div>
+        </form>
       <% end %>
     </div>
     """
   end
 
-  def handle_event("modal-open", _unsigned_params, socket) do
-    {:noreply, assign(socket, show_modal: true)}
+  def handle_event("open-modal", %{"id" => id}, socket) do
+    send_update(BordoWeb.Components.Modal, id: id, state: "OPEN")
+    {:noreply, socket}
   end
 
-  def handle_event("modal-close", _unsigned_params, socket) do
-    {:noreply, assign(socket, show_modal: false)}
+  def handle_event("close-modal", %{"id" => id}, socket) do
+    # SO THE CSS ANIMATIONS HAVE TIME TO RUN
+    :timer.sleep(100)
+    send_update(BordoWeb.Components.Modal, id: id, state: "CLOSED", action: nil)
+    {:noreply, socket}
   end
 
   def handle_event("save", %{"brand" => brand_params}, socket) do
-    owner_id = socket.assigns.user_id
-
-    case Brands.create_brand(brand_params |> Enum.into(%{"owner_id" => owner_id})) do
+    case Brands.create_brand(brand_params) do
       {:ok, brand} ->
-        team_id = socket.assigns.team_id
-        Brands.create_brand_team(%{team_id: team_id, brand_id: brand.id})
+        Brands.create_brand_team(%{team_id: brand_params["team_id"], brand_id: brand.id})
 
         {:noreply,
          socket
