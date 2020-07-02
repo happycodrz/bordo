3ab1fe7d151ec3844e9cf65b9e6af44f59396aea
Kevin Brown <chevinbrown@gmail.com>
2020-07-01 19:12:29 -0500

    Add virtual password so we can validate

--- lib/bordo/users/user.ex
+++ lib/bordo/users/user.ex
@@ -8,6 +8,7 @@ defmodule Bordo.Users.User do
     field :first_name, :string
     field :last_name, :string
     field :image_url, :string
+    field :password, :string, virtual: true
 
     belongs_to :team, Bordo.Teams.Team
     timestamps()
@@ -16,8 +17,18 @@ defmodule Bordo.Users.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:email, :auth0_id, :team_id, :image_url, :first_name, :last_name, :team_id])
+    |> cast(attrs, [
+      :email,
+      :auth0_id,
+      :team_id,
+      :image_url,
+      :first_name,
+      :last_name,
+      :team_id,
+      :password
+    ])
     |> validate_required([:email])
+    |> validate_length(:password, min: 8)
     |> unique_constraint(:email)
   end
 end
