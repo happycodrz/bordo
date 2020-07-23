46130ad505ba2472c3d36e1a61e61cb257641731
Kevin Brown <chevinbrown@gmail.com>
2020-07-23 18:42:58 -0500

    Push event after timeout

--- assets/js/hooks/slide_over.js
+++ assets/js/hooks/slide_over.js
@@ -3,7 +3,9 @@ export const initSlideOver = {
     const handleOpenCloseEvent = (event) => {
       if (event.detail.open === false) {
         this.el.removeEventListener('slideover-change', handleOpenCloseEvent)
-        this.pushEvent('close-slideover', { id: this.el.id })
+        setTimeout(() => {
+          this.pushEvent('close-slideover', { id: this.el.id })
+        }, 300)
       }
     }
     this.el.addEventListener('slideover-change', handleOpenCloseEvent)
