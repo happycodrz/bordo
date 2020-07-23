8fb57a9182c42e37b7a21b7dc9d1c484ad3606ae
Kevin Brown <chevinbrown@gmail.com>
2020-07-23 18:43:40 -0500

    Support escape-key, sizes, and overlay

--- lib/bordo_web/live/components/slide_over.ex
+++ lib/bordo_web/live/components/slide_over.ex
@@ -13,15 +13,27 @@ defmodule BordoWeb.Components.SlideOver do
         phx-hook="initSlideOver"
         x-data="{ open: false }"
         x-init="() => {
-          setTimeout(() => open = true, 100);
-          $watch('open', isOpen => $dispatch('slideover-change', { open: isOpen }))
-        }"
-        x-show="open"
-        close-slideover="setTimeout(() => open = false, 100)"
-        class="fixed inset-0 overflow-hidden z-20">
+            setTimeout(() => open = true, 100);
+            $watch('open', isOpen => $dispatch('slideover-change', { open: isOpen }))
+          }"
+        @close-slideover="setTimeout(() => open = false, 100)"
+        @keydown.window.escape="open = false; setTimeout(() => open = true, 100);"
+        class="fixed inset-0 z-20 overflow-hidden">
         <div class="absolute inset-0 overflow-hidden">
-          <section @click.away="open = false; setTimeout(() =>open = true, 1000);" class="absolute inset-y-0 right-0 pl-10 max-w-full flex">
-            <div class="w-screen max-w-md" x-description="Slide-over panel, show/hide based on slide-over state." x-show="open" x-transition:enter="transform transition ease-in-out duration-500 sm:duration-300" x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transform transition ease-in-out duration-500 sm:duration-300" x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full">
+          <%= if assigns[:with_overlay] do %>
+            <div x-show="open" x-description="Background overlay, show/hide based on slide-over state."
+              x-transition:enter="ease-in-out duration-100" x-transition:enter-start="opacity-0"
+              x-transition:enter-end="opacity-100" x-transition:leave="ease-in-out duration-100"
+              x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
+              class="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+            <% end %>
+          <section @click.away="open = false; setTimeout(() => open = true, 100);"
+            class="absolute inset-y-0 right-0 pl-10 max-w-full flex">
+            <div class="w-screen <%= size_class(assigns[:size]) %>" x-description="Slide-over panel, show/hide based on slide-over state."
+              x-show="open" x-transition:enter="transform transition ease-in-out duration-100"
+              x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0"
+              x-transition:leave="transform transition ease-in-out duration-100" x-transition:leave-start="translate-x-0"
+              x-transition:leave-end="translate-x-full">
               <div class="h-full flex flex-col space-y-6 py-6 bg-white shadow-xl overflow-y-scroll">
                 <header class="px-4 sm:px-6">
                   <div class="flex items-start justify-between space-x-3">
@@ -29,15 +41,19 @@ defmodule BordoWeb.Components.SlideOver do
                       <%= @title %>
                     </h2>
                     <div class="h-7 flex items-center">
-                      <button @click="open = false; setTimeout(() =>open = true, 1000);" aria-label="Close panel" class="text-gray-400 hover:text-gray-500 transition ease-in-out duration-150">
+                      <button @click="open = false; setTimeout(() => open = true, 100);" aria-label="Close panel"
+                        class="text-gray-400 hover:text-gray-500 transition ease-in-out duration-100">
                         <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12">
+                          </path>
                         </svg>
                       </button>
                     </div>
                   </div>
                 </header>
-                <%= @inner_content.([]) %>
+                <div class="relative flex-1 px-4 sm:px-6">
+                  <%= @inner_content.([]) %>
+                </div>
               </div>
             </div>
           </section>
@@ -45,5 +61,21 @@ defmodule BordoWeb.Components.SlideOver do
       </div>
     </div>
     """
+  end
+
+  defp size_class(size) do
+    case size do
+      "small" ->
+        "max-w-md"
+
+      "medium" ->
+        "max-w-lg"
+
+      "large" ->
+        "max-w-2xl"
+
+      _ ->
+        "max-w-md"
+    end
   end
 end
