1651582edb1b011186908958d19eae2cb7455c06
Kevin Brown <chevinbrown@gmail.com>
2020-09-10 07:19:26 -0500

    Add basic counter hook, jank AF

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,9 +37,27 @@ Hooks.FeatherIcon = {
 Hooks.DatePicker = {
   mounted() {
     flatpickr(this.el, {
-      enableTime: true
-    });
-  }
+      enableTime: true,
+    })
+  },
+}
+Hooks.TwitterLimit = {
+  textAreaUpdated(event, helpText) {
+    helpText.textContent = `${event.target.textLength}/280`
+  },
+  mounted() {
+    console.log('mounted')
+    const helpText = document.createElement('p')
+    helpText.classList.add('mt-2', 'text-sm', 'text-gray-500')
+    helpText.textContent = '0/280'
+    this.el.parentNode.parentNode.appendChild(helpText)
+    this.el.addEventListener('keyup', (e) => {
+      this.textAreaUpdated(e, helpText)
+    })
+  },
+  destroyed() {
+    this.el.removeEventListener('keyup', this.textAreaUpdated)
+  },
 }
 
 Hooks = { ...Hooks, InitModal, initSlideOver, Toast }
