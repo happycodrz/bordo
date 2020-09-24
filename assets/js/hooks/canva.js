b44c88ddcd48c5d6a62e6df02569d4a651de05ca
Kevin Brown <chevinbrown@gmail.com>
2020-09-24 06:31:06 -0500

    Hacky-put canva on window

--- assets/js/hooks/canva.js
+++ assets/js/hooks/canva.js
@@ -1,30 +1,31 @@
 export const Canva = {
   mounted() {
-    if (!document.getElementById('canva-script')) {
+    if (window.CanvaButton === undefined) {
       this.loadCanva()
     }
     this.handleEvent('open', () => {
       this.createDesign()
     })
   },
-  destroyed() {},
+  destroyed() {
+
+  },
   loadCanva() {
     let script = document.createElement('script')
     script.src = 'https://sdk.canva.com/v2/beta/api.js'
-    script.id = 'canva-script'
     script.onload = () => {
       // API initialization
       window.CanvaButton.initialize({
         apiKey: 'fWzIitASrQlpVDa-nh7oUNl-',
       }).then((api) => {
-        this.canva = api
+        window.canva = api
       })
     }
 
     document.body.appendChild(script)
   },
   createDesign() {
-    this.canva.createDesign({
+    window.canva.createDesign({
       publishLabel: 'Upload to Bordo',
       type: 'SocialMedia',
       onDesignPublish: ({
