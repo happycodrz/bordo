d6ea7a2f3754a0a5fdc5ce0a36160d8b8f0d1f1a
Kevin Brown <chevinbrown@gmail.com>
2020-09-26 16:34:33 -0500

    Reconfigure to use migrator & non-dynamic repo

--- lib/bordo/repo.ex
+++ lib/bordo/repo.ex
@@ -5,8 +5,4 @@ defmodule Bordo.Repo do
     pool_size: 10
 
   use Scrivener, page_size: 10
-
-  def init(_type, config) do
-    {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
-  end
 end
