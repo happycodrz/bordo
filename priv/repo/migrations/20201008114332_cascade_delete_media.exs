771573226d372b4529ce5a3b1b24e9fea14498d1
Kevin Brown <chevinbrown@gmail.com>
2020-10-08 19:12:38 -0500

    Format

--- priv/repo/migrations/20201008114332_cascade_delete_media.exs
+++ priv/repo/migrations/20201008114332_cascade_delete_media.exs
@@ -3,7 +3,8 @@ defmodule Bordo.Repo.Migrations.CascadeDeleteMedia do
 
   def change do
     alter table(:media) do
-      modify :brand_id, references(:brands, column: :id, type: :uuid, on_delete: :delete_all), from: references(:brands)
+      modify :brand_id, references(:brands, column: :id, type: :uuid, on_delete: :delete_all),
+        from: references(:brands)
     end
   end
 end
