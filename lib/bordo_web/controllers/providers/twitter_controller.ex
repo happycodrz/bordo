7ce0091fdf741f1a358125731fe0360f71c24b76
Kevin Brown <chevinbrown@gmail.com>
2020-11-10 08:34:15 -0600

    Token-response changed...

--- lib/bordo_web/controllers/providers/twitter_controller.ex
+++ lib/bordo_web/controllers/providers/twitter_controller.ex
@@ -13,7 +13,7 @@ defmodule BordoWeb.Providers.TwitterController do
   def auth(conn, %{"brand_id" => brand_id}) do
     configure_twitter()
 
-    with {:ok, token} <- get_request_token(brand_id),
+    with {:ok, {:ok, token}} <- get_request_token(brand_id),
          {:ok, authenticate_url} <- get_authenticate_url(token) do
       redirect(conn, external: authenticate_url)
     else
@@ -71,7 +71,7 @@ defmodule BordoWeb.Providers.TwitterController do
 
   # Handling errors from extwitter request_token is difficult right now b/c no error tuple is returned
   # this could be refactored later to return the error tuple.
-  defp get_request_token(brand_id) do
+  def get_request_token(brand_id) do
     query = URI.encode_query(%{brand_id: brand_id})
 
     try do
@@ -90,7 +90,7 @@ defmodule BordoWeb.Providers.TwitterController do
     end
   end
 
-  defp get_authenticate_url(tokens) do
+  def get_authenticate_url(tokens) do
     tokens
     |> Map.get(:oauth_token)
     |> ExTwitter.authenticate_url()
